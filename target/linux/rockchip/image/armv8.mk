415e97eae439ae9a257bcaf6f02c5208905e4fea
Hsun Lai <i@chainsx.cn>
2025-06-11 03:19:11 +0800

    rockchip: sakurapi-rk3308b: add usbgadget packages, add rockchip-drm packages
    Signed-off-by: chainsx <i@chainsx.cn>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -243,7 +243,7 @@ define Device/sakurapi_sakura-pi
   SUPPORTED_DEVICES := sakurapi,rk3308b
   DEVICE_DTS := rockchip/rk3308-sakurapi-rk3308b
   UBOOT_DEVICE_NAME := sakura-pi-rk3308
-  DEVICE_PACKAGES := kmod-brcmfmac brcmfmac-nvram-43455-sdio cypress-firmware-43455-sdio kmod-usb-net-cdc-ncm kmod-usb-net-rndis wpad-basic-mbedtls
+  DEVICE_PACKAGES := kmod-brcmfmac brcmfmac-nvram-43455-sdio cypress-firmware-43455-sdio wpad-basic-mbedtls usb-modeswitch usbgadget usbgadget-ncm
 endef
 TARGET_DEVICES += sakurapi_sakura-pi
 
